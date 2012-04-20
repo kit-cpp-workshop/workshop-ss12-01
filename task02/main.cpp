88e96e1846a9891fba38dc592bdb56c24728834f
Robert Schneider <robert.schneider@cuviso.de>
2012-04-20 19:10:34 +0200

    Adds eclipse projects, line feeds before EOL

--- task02/main.cpp
+++ task02/main.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main()
 {
 	int n = 0;
-	cout << "How many numbers? ";
+	cout << "How many numbers? " << flush;
 	cin >> n;
 	
 	// Insert your code here
 	
 	return 0;
 }
-
