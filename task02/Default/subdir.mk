18225e2bad7d4787d7b171c77fcf13f0365bf4b5
rschneider <robert.schneider@cuviso.de>
2012-04-22 15:59:10 +0200

    Trying with makefiles

---
+++ task02/Default/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../task02.cpp 
+
+OBJS += \
+./task02.o 
+
+CPP_DEPS += \
+./task02.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
