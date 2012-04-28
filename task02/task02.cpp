b034d5e64a6d5e2a77adfc24ee368edc4a82e843
primeminister89 <prime@leech.it>
2012-04-28 15:07:38 +0200

    Aufgabe 2 bearbeitet.

--- task02/task02.cpp
+++ task02/task02.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 
-using namespace std;
+//using namespace std;
+
+int fibo(int x); // Prototyp
 
 int main()
 {
 	int n = 0;
-	cout << "How many numbers? " << flush;
-	cin >> n;
+	std::cout << "How many numbers? " << std::flush;
+	std::cin >> n;
 	
 	// Insert your code here
-	
-	return 0;
+	for (int i=1; i<=n; ++i) {
+		std::cout << fibo(i) << std::endl;
+	}
+
+	std::cin.get();
+}
+
+int fibo(int x) {
+	if (x == 1) {
+		return 0;
+	} else if (x == 2) {
+		return 1;
+	} else {
+		return fibo(x-1) + fibo(x-2);
+	}
 }
