9d9514f7fafabe5151775916164223bf62581b57
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-02 21:16:45 +0200

    Algorithmus zur Berechnung der Fibonacci-Folge mit Ausgabe hinzugefügt.

--- task02/task02.cpp
+++ task02/task02.cpp
@@ -16,12 +16,19 @@ int main()
 
 	int numbers[3] = {0,1,1}; // the last two and the current fibonacci number
 
-	for(int i=0; i<=n; i++) {
-
-
-
+	for(int i=0; i<n; i++) {
+		cout << "Fibonacci number " << i << " is ";
+		if(i<3) cout << numbers[i];
+		else {
+			//shift numbers and calculate new one
+			numbers[0] = numbers[1];
+			numbers[1] = numbers[2];
+			numbers[2] = numbers[0] + numbers[1];
+			cout << numbers[2];
+		}
+		cout << endl;
 	}
-	// Insert your code here
+	cout << endl << "Done." << endl;
 	
 	return 0;
 }
