3e54d397b2004863bd5dd931b5486c4800eab005
Christian Buntin <christian.buntin@student.kit.edu>
2012-05-02 21:08:31 +0200

    Grundgerüst mit Eingabe und Überprüfung erstellt

--- task02/task02.cpp
+++ task02/task02.cpp
@@ -8,6 +8,19 @@ int main()
 	cout << "How many numbers? " << flush;
 	cin >> n;
 	
+	// check for correct input
+	if(n<=0) {
+		cout << "Please enter valid number!" << endl;
+		return 0;
+	}
+
+	int numbers[3] = {0,1,1}; // the last two and the current fibonacci number
+
+	for(int i=0; i<=n; i++) {
+
+
+
+	}
 	// Insert your code here
 	
 	return 0;
