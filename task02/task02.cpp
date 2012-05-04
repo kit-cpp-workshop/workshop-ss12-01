e72057f86296452aceeae6e79833653d0a10fe81
Markus Jung <markus.jung@stud.uni-karlsruhe.de>
2012-05-04 13:10:13 +0200

    Add code to calculate fibonacci number sequence

--- task02/task02.cpp
+++ task02/task02.cpp
@@ -8,7 +8,22 @@ int main()
 	cout << "How many numbers? " << flush;
 	cin >> n;
 	
-	// Insert your code here
+	int lastFibos[2] = { 0, 1 };
+	int i;
+
+	// Easy case: first two fibos
+	for (i = 0; i < 2 && i < n; i++) {
+	    cout << lastFibos[i] << endl;
+	}
+
+	// This case is easy, too
+	for (; i < n; i++) {
+	    const int newFibo = lastFibos[0] + lastFibos[1];
+
+	    lastFibos[i & 0x1] = newFibo;
+
+	    cout << newFibo << endl;
+	}
 	
 	return 0;
 }
